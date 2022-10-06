9c8bee8deb51f65e60da1b7e1073ba271a38e272
dominic-wassef <domwassef@gmail.com>
2022-10-05 23:58:26 -0400

    adding randomString function

--- helpers.go
+++ helpers.go
@@ -1,6 +1,24 @@
 package ghostly
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321_+"
+)
+
+// Generates a randomString
+func (c *Ghostly) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
 
 func (c *Ghostly) CreateDirIfNotExist(path string) error {
 	const mode = 0755
