5e342e2f814c8daa8093b9e88b741c8ce6be4ef2
dominic-wassef <domwassef@gmail.com>
2022-10-09 23:29:23 -0400

    task: create db migration support file

---
+++ cmd/cli/migrate.go
@@ -0,0 +1,39 @@
+package main
+
+func doMigrate(arg2, arg3 string) error {
+	dsn := getDSN()
+
+	// run the migration command
+	switch arg2 {
+	case "up":
+		err := gho.MigrateUp(dsn)
+		if err != nil {
+			return err
+		}
+
+	case "down":
+		if arg3 == "all" {
+			err := gho.MigrateDownAll(dsn)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := gho.Steps(-1, dsn)
+			if err != nil {
+				return err
+			}
+		}
+	case "reset":
+		err := gho.MigrateDownAll(dsn)
+		if err != nil {
+			return err
+		}
+		err = gho.MigrateUp(dsn)
+		if err != nil {
+			return err
+		}
+	default:
+		showHelp()
+	}
+	return nil
+}
