45eb9cc56aca3cc618acf9a54de7cba06ca95fb5
dominic-wassef <domwassef@gmail.com>
2022-10-09 23:26:04 -0400

    task: create resp util to writejson

---
+++ response-utils.go
@@ -0,0 +1,26 @@
+package ghostly
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (g *Ghostly) WriteJson(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
