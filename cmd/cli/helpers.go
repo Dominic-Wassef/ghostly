b58801bba056b3fe07e553c2521201bf1c215e40
dominic-wassef <domwassef@gmail.com>
2022-10-10 21:48:21 -0400

    // cli command config

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -64,5 +64,8 @@ func showHelp() {
 	migrate reset - runs all down migrations in reverse order, and then all up migrations
 	make migration <name> - creates two new up and down migrations in the migrations folder
 	make auth - creates and runs migrations for authentication tables, and creates models and middleware
+	make handler <name> - creates a sub handler in the handlers directory
+	make model <name> - creates a new model in the data directory
+	make session - creates a table in the database as a session store
 	`)
 }
