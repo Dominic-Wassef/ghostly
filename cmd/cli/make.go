82c829742fc79b63ca10eddeb7ff65950319396e
dominic-wassef <domwassef@gmail.com>
2022-10-10 21:48:42 -0400

    handler || model case

--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -3,7 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"time"
+
+	"github.com/gertd/go-pluralize"
+	"github.com/iancoleman/strcase"
 )
 
 func doMake(arg2, arg3 string) error {
@@ -32,6 +37,72 @@ func doMake(arg2, arg3 string) error {
 
 	case "auth":
 		err := doAuth()
+		if err != nil {
+			exitGracefully(err)
+		}
+
+	case "handler":
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the handler a name"))
+		}
+
+		fileName := gho.RootPath + "/handlers/" + strings.ToLower(arg3) + ".go"
+		if fileExists(fileName) {
+			exitGracefully(errors.New(fileName + " already exists!"))
+		}
+
+		data, err := templateFS.ReadFile("templates/handlers/handler.go.txt")
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		handler := string(data)
+		handler = strings.ReplaceAll(handler, "$HANDLERNAME$", strcase.ToCamel(arg3))
+
+		err = ioutil.WriteFile(fileName, []byte(handler), 0644)
+		if err != nil {
+			exitGracefully(err)
+		}
+
+	case "model":
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the model a name"))
+		}
+
+		data, err := templateFS.ReadFile("templates/data/model.go.txt")
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		model := string(data)
+
+		plur := pluralize.NewClient()
+
+		var modelName = arg3
+		var tableName = arg3
+
+		if plur.IsPlural(arg3) {
+			modelName = plur.Singular(arg3)
+			tableName = strings.ToLower(tableName)
+		} else {
+			tableName = strings.ToLower(plur.Plural(arg3))
+		}
+
+		fileName := gho.RootPath + "/data/" + strings.ToLower(modelName) + ".go"
+		if fileExists(fileName) {
+			exitGracefully(errors.New(fileName + " already exists!"))
+		}
+
+		model = strings.ReplaceAll(model, "$MODELNAME$", strcase.ToCamel(modelName))
+		model = strings.ReplaceAll(model, "$TABLENAME$", tableName)
+
+		err = copyDataToFile([]byte(model), fileName)
+		if err != nil {
+			exitGracefully(err)
+		}
+
+	case "session":
+		err := doSessionTable()
 		if err != nil {
 			exitGracefully(err)
 		}
