bb851f191942009b870c4071ad43c1bcfa387916
dominic-wassef <domwassef@gmail.com>
2022-10-10 21:48:50 -0400

    task: sessions

---
+++ cmd/cli/session.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doSessionTable() error {
+	dbType := gho.DB.DataType
+
+	if dbType == "mariadb" {
+		dbType = "mysql"
+	}
+
+	if dbType == "postgresql" {
+		dbType = "postgres"
+	}
+
+	fileName := fmt.Sprintf("%d_create_sessions_table", time.Now().UnixMicro())
+	upFile := gho.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
+	downFile := gho.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
+
+	err := copyFileFromTemplate("templates/migrations/"+dbType+"_session.sql", upFile)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyDataToFile([]byte("drop table sessions"), downFile)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = doMigrate("up", "")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	return nil
+}
