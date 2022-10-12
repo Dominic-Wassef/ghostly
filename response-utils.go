edaaad1bea8c9e1bb251ca646f6b99beae50b6f4
dominic-wassef <domwassef@gmail.com>
2022-10-11 23:57:05 -0400

    support: XML and Downloads

--- response-utils.go
+++ response-utils.go
@@ -2,7 +2,11 @@ package ghostly
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
+	"path/filepath"
 )
 
 func (g *Ghostly) WriteJson(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -23,4 +27,58 @@ func (g *Ghostly) WriteJson(w http.ResponseWriter, status int, data interface{},
 		return err
 	}
 	return nil
+}
+
+// support for xml
+func (g *Ghostly) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// support to serve a file that must be downloaded
+func (g *Ghostly) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
+	fp := path.Join(pathToFile, fileName)
+	// security check
+	fileToServe := filepath.Clean(fp)
+	// setting header type
+	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	http.ServeFile(w, r, fileToServe)
+	return nil
+}
+
+// Response for errors
+func (g *Ghostly) Error404(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusNotFound)
+}
+
+func (g *Ghostly) Error500(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusInternalServerError)
+}
+
+func (g *Ghostly) ErrorUnauthorized(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+func (g *Ghostly) ErrorForbidden(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusForbidden)
+}
+
+// generic func
+func (g *Ghostly) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
