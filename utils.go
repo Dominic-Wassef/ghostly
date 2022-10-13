120c9a280386a17cdc04c8579103f1414866b44f
dominic-wassef <domwassef@gmail.com>
2022-10-13 17:42:21 -0400

    adding: test func for LoadTime

---
+++ utils.go
@@ -0,0 +1,17 @@
+package ghostly
+
+import (
+	"fmt"
+	"regexp"
+	"runtime"
+	"time"
+)
+
+func (g *Ghostly) LoadTime(start time.Time) {
+	elapsed := time.Since(start)
+	pc, _, _, _ := runtime.Caller(1)
+	funcObj := runtime.FuncForPC(pc)
+	runtimeFunc := regexp.MustCompile(`^.*\.(.*)$`)
+	name := runtimeFunc.ReplaceAllString(funcObj.Name(), "$1")
+	g.InfoLog.Println(fmt.Sprintf("Load time: %s took %s", name, elapsed))
+}
