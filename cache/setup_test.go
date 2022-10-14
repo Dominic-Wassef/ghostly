90719632db89f9a55eb56b1e3d7d7fa7c7c727ff
dominic-wassef <domwassef@gmail.com>
2022-10-13 22:49:23 -0400

    task: setting up test for badger

--- cache/setup_test.go
+++ cache/setup_test.go
@@ -1,15 +1,18 @@
 package cache
 
 import (
+	"log"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/gomodule/redigo/redis"
 )
 
 var testRedisCache RedisCache
+var testBadgerCache BadgerCache
 
 func TestMain(m *testing.M) {
 	s, err := miniredis.Run()
@@ -31,5 +34,20 @@ func TestMain(m *testing.M) {
 	testRedisCache.Prefix = "test-ghostly"
 
 	defer testRedisCache.Conn.Close()
+	_ = os.RemoveAll("./testdata/tmp/badger")
+	// create a badger DB
+	if _, err := os.Stat("./testdata/tmp"); os.IsNotExist(err) {
+		err := os.Mkdir("./testdata/tmp", 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	// making a badger folder
+	err = os.Mkdir("./testdata/tmp/badger", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, _ := badger.Open(badger.DefaultOptions("./testdata/tmp/badger"))
+	testBadgerCache.Conn = db
 	os.Exit(m.Run())
 }
