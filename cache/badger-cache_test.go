314e563463fcb0ff9363b72793d8ca0f291b5cfd
dominic-wassef <domwassef@gmail.com>
2022-10-13 22:49:31 -0400

    badgeDB test

---
+++ cache/badger-cache_test.go
@@ -0,0 +1,131 @@
+package cache
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBadgerCache_Has(t *testing.T) {
+	err := testBadgerCache.Forget("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	inCache, err := testBadgerCache.Has("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	if inCache {
+		t.Error("foo found in cache when it shouldn't be there")
+	}
+	_ = testBadgerCache.Set("foo", "bar")
+	inCache, err = testBadgerCache.Has("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	if !inCache {
+		t.Error("foo not found in cache")
+	}
+	err = testBadgerCache.Forget("foo")
+	if err != nil {
+		fmt.Println("NULL")
+	}
+}
+
+func TestBadgerCache_Get(t *testing.T) {
+	err := testBadgerCache.Set("foo", "bar")
+	if err != nil {
+		t.Error(err)
+	}
+
+	x, err := testBadgerCache.Get("foo")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if x != "bar" {
+		t.Error("did not get correct value from cache")
+	}
+}
+
+func TestBadgerCache_Forget(t *testing.T) {
+	err := testBadgerCache.Set("foo", "foo")
+	if err != nil {
+		t.Error(err)
+	}
+	err = testBadgerCache.Forget("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	inCache, err := testBadgerCache.Has("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	if inCache {
+		t.Error("foo found in cache when it shouldn't be there")
+	}
+}
+
+func TestBadgerCache_Empty(t *testing.T) {
+	err := testBadgerCache.Set("alpha", "beta")
+	if err != nil {
+		t.Error(err)
+	}
+	err = testBadgerCache.Empty()
+	if err != nil {
+		t.Error(err)
+	}
+
+	inCache, err := testBadgerCache.Has("alpha")
+	if err != nil {
+		t.Error(err)
+	}
+	if inCache {
+		t.Error("alpha found in cache when it shouldn't be there")
+	}
+}
+
+func TestBadgerCache_EmptyByMatch(t *testing.T) {
+	err := testBadgerCache.Set("alpha", "beta")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testBadgerCache.Set("alpha2", "beta2")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testBadgerCache.Set("beta", "beta")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testBadgerCache.EmptyByMatch("a")
+	if err != nil {
+		t.Error(err)
+	}
+
+	inCache, err := testBadgerCache.Has("alpha")
+	if err != nil {
+		t.Error(err)
+	}
+	if inCache {
+		t.Error("alpha found in cache when it shouldn't be there")
+	}
+
+	inCache, err = testBadgerCache.Has("alpha2")
+	if err != nil {
+		t.Error(err)
+	}
+	if inCache {
+		t.Error("alpha2 found in cache when it shouldn't be there")
+	}
+
+	inCache, err = testBadgerCache.Has("beta")
+	if err != nil {
+		t.Error(err)
+	}
+	if !inCache {
+		t.Error("beta not found in cache when it should be there")
+	}
+}
