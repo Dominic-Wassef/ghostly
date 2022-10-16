a98aba2cd57c70a79e419273161641b2950daca2
dominic-wassef <domwassef@gmail.com>
2022-10-15 23:21:57 -0400

    fix: send func

--- mailer/mail.go
+++ mailer/mail.go
@@ -59,7 +59,7 @@ func (m *Mail) ListenForMail() {
 
 func (m *Mail) Send(msg Message) error {
 	if len(m.API) > 0 && len(m.APIKey) > 0 && len(m.APIUrl) > 0 && m.API != "smtp" {
-		m.ChooseAPI(msg)
+		return m.ChooseAPI(msg)
 	}
 	return m.SendSMTPMessage(msg)
 }
