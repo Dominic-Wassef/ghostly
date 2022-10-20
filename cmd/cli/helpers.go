abafda84c06ee084cdb39b23f5fd2bb8c64ea7d3
dominic-wassef <domwassef@gmail.com>
2022-10-20 01:00:20 -0400

    task: create updateSourceFiles func for proj init

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -3,24 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/joho/godotenv"
 )
 
-func setup() {
-	err := godotenv.Load()
-	if err != nil {
-		exitGracefully(err)
-	}
+func setup(arg1, arg2 string) {
+	if arg1 != "new" && arg1 != "version" && arg1 != "help" {
+		err := godotenv.Load()
+		if err != nil {
+			exitGracefully(err)
+		}
 
-	path, err := os.Getwd()
-	if err != nil {
-		exitGracefully(err)
-	}
+		path, err := os.Getwd()
+		if err != nil {
+			exitGracefully(err)
+		}
 
-	gho.RootPath = path
-	gho.DB.DataType = os.Getenv("DATABASE_TYPE")
+		gho.RootPath = path
+		gho.DB.DataType = os.Getenv("DATABASE_TYPE")
+	}
 }
 
 func getDSN() string {
@@ -69,4 +73,44 @@ func showHelp() {
 	make session - creates a table in the database as a session store
 	make mail <name> - creates two starter mail templates in the mail directory
 	`)
+}
+
+func updateSourceFiles(path string, fi os.FileInfo, err error) error {
+	// check for an error before doing anything else
+	if err != nil {
+		return err
+	}
+	// check if current file is directory
+	if fi.IsDir() {
+		return nil
+	}
+	// only check go file
+	matched, err := filepath.Match("*.go", fi.Name())
+	if err != nil {
+		return err
+	}
+	// we have a matching file
+	if matched {
+		// read file contents
+		read, err := os.ReadFile(path)
+		if err != nil {
+			exitGracefully(err)
+		}
+		newContents := strings.Replace(string(read), "myapp", appURL, -1)
+		// write the changed file
+		err = os.WriteFile(path, []byte(newContents), 0)
+		if err != nil {
+			exitGracefully(err)
+		}
+	}
+
+	return nil
+}
+
+func updateSource() {
+	// walk entire project folder, including subfolders
+	err := filepath.Walk(".", updateSourceFiles)
+	if err != nil {
+		exitGracefully(err)
+	}
 }
