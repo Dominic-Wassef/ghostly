f5bd9b410d17af72f82a9d73de9f176dfe53e604
dominic-wassef <domwassef@gmail.com>
2022-10-20 01:00:50 -0400

    feat: "ghostly new <appname>" now working

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -19,11 +19,17 @@ func main() {
 		exitGracefully(err)
 	}
 
-	setup()
+	setup(arg1, arg2)
 
 	switch arg1 {
 	case "help":
 		showHelp()
+
+	case "new":
+		if arg2 == "" {
+			exitGracefully(errors.New("new requires an application name"))
+		}
+		doNew(arg2)
 
 	case "version":
 		color.Yellow("Application version: " + version)
