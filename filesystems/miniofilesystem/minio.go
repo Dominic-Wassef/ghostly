8fb98b0c693f9063d9840524e6c37de750ed3079
dominic-wassef <domwassef@gmail.com>
2022-10-23 04:10:06 -0400

    In progress: Minio

---
+++ filesystems/miniofilesystem/minio.go
@@ -0,0 +1,46 @@
+package miniofilesystem
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"path"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type Minio struct {
+	Endpoint string
+	Key      string
+	Secret   string
+	UseSSL   bool
+	Region   string
+	Bucket   string
+}
+
+func (m *Minio) getCredentials() *minio.Client {
+	client, err := minio.New(m.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(m.Key, m.Secret, ""),
+		Secure: m.UseSSL,
+	})
+	if err != nil {
+		log.Println(err)
+	}
+	return client
+}
+
+func (m *Minio) Put(fileName, folder string) error {
+	ctx, canel := context.WithCancel(context.Background())
+	defer canel()
+	objectName := path.Base(fileName)
+	client := m.getCredentials()
+	uploadInfo, err := client.FPutObject(ctx, m.Bucket, fmt.Sprintf("%s/%s", folder, objectName), fileName, minio.PutObjectOptions{})
+	if err != nil {
+		log.Println("Failed with FPutObject")
+		log.Println(err)
+		log.Println("uploadInfo:", uploadInfo)
+		return err
+	}
+	return nil
+}
